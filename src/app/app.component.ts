ab0b3cd9df2e7265f7b9485e0a61f344d0b7b431
sopelan <marcossopelana@gmail.com>
2021-09-01 15:27:52 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejercicio1lab4';
+  Edad1 : number;
+  Edad2 : number;
+  Suma : number;
+  Promedio : number;
+  constructor()
+  {
+    this.Edad1 = 0;
+    this.Edad2 = 0;
+    this.Suma = 0;
+    this.Promedio = 0;
+  }
+  Calcular()
+  { 
+    this.Suma = this.Edad1 + this.Edad2;
+    this.Promedio = this.Suma/2;
+  }
+  Limpiar()
+  {
+    this.Edad1 = 0;
+    this.Edad2 = 0;
+    this.Suma = 0;
+    this.Promedio = 0;
+  }
 }
